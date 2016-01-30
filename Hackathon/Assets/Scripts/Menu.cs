afc8cec87f2e6c36c2cb63f9d4bbebc2239837a9
Mohamed Mansour <hello@mohamedmansour.com>
2016-01-29 22:56:49 -0800

    Create Menu Basic Background

---
+++ Hackathon/Assets/Scripts/Menu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Attach to Main Camera
+/// </summary>
+public class Menu : MonoBehaviour {
+    public Texture backgroundTexture;
+
+    void OnGUI()
+    {
+        // Display our background texture.
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgroundTexture);
+
+
+    }
+
+
+}
