7dd31654522f152bd677dd21a25ace99b22f2377
Mohamed Mansour <hello@mohamedmansour.com>
2016-01-29 23:51:07 -0800

    Create Menu Button to new Scene

--- Hackathon/Assets/Scripts/Menu.cs
+++ Hackathon/Assets/Scripts/Menu.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// Attach to Main Camera
@@ -10,10 +11,15 @@ public class Menu : MonoBehaviour {
     void OnGUI()
     {
         // Display our background texture.
-        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgroundTexture);
+        //GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgroundTexture);
 
-
+        // Display buttons
+        //GUI.Button(new Rect(Screen.width * .5f, Screen.height * 0.5f, Screen.width * .5f, Screen.height * .1f), "Plays");
     }
 
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("Main");
+    }
 
 }
