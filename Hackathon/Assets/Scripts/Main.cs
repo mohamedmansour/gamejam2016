5412c702f5ac35ebd53650c5bf7980a2debb5478
Mohamed Mansour <hello@mohamedmansour.com>
2016-01-30 19:25:49 -0800

    Bind some UI Components to main

--- Hackathon/Assets/Scripts/Main.cs
+++ Hackathon/Assets/Scripts/Main.cs
@@ -1,7 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Main : MonoBehaviour {
+
+    public Image imagePanel;
+    public GameObject instructionsPanel;
+    public GameObject componentPanel;
+    public Text orderText;
+    
 
     void OnGUI()
     {
