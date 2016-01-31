708d4af6c160d018080e88d7e55191ea8f312b74
Mohamed Mansour <hello@mohamedmansour.com>
2016-01-30 23:28:24 -0800

    Progress

--- Hackathon/Assets/Scripts/Main.cs
+++ Hackathon/Assets/Scripts/Main.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using Assets.Scripts.Extensions;
 
 public class Main : MonoBehaviour {
 
@@ -13,7 +14,14 @@ public class Main : MonoBehaviour {
     public Image instruction4Image;
     public GameObject componentPanel;
     public Text orderText;
-    
+    public Image loadingPanel;
+
+    private float x;
+    private float y;
+    private Vector2 resolution;
+
+    private int currentRoundTime = 0;
+    private int maxRoundTime = 8;
 
     void OnGUI()
     {
@@ -22,12 +30,39 @@ public class Main : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
+        resolution = new Vector2(Screen.width, Screen.height);
+        x = Screen.width / 1080.0f; // 1920 is the x value of the working resolution (as described in the first point)
+        y = Screen.height / 1920.0f; // 1042 is the y value of the working resolution (as described in the first point)
+
         orderText.text = "Change Me";
 
+        InvokeRepeating("OnTickUpdate", 0, 1.0f);
     }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (Screen.width != resolution.x || Screen.height != resolution.y)
+        {
+            resolution = new Vector2(Screen.width, Screen.height);
+            x = resolution.x / 1080.0f;
+            y = resolution.y / 1920.0f;
+        }
+    }
+
+    void OnTickUpdate()
+    {
+        var currentWidth = 1080.0f * (1.0f / maxRoundTime);
+        currentRoundTime++;
+
+        //-1080 -> 1080
+        // 0
+        loadingPanel.rectTransform.position = new Vector3(currentWidth + loadingPanel.rectTransform.position.x, loadingPanel.transform.position.y);
+        Debug.Log(currentWidth);
+        //loadingPanel.transform.Translate(new Vector3(currentWidth, loadingPanel.transform.position.y));
+        //loadingPanel.rectTransform.SetRightTopPosition(new Vector2(currentWidth, 0));
+        if (currentRoundTime == maxRoundTime)
+        {
+            currentRoundTime = 0;
+        }
+    }
 }
