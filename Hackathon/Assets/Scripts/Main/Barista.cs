39d5b0e5cdcca24b8e2486d4f16a5fca8ecc0e7e
Michael Johnston <mjohnst@microsoft.com>
2016-01-30 21:43:26 -0800

    Add get actions to barista

--- Hackathon/Assets/Scripts/Main/Barista.cs
+++ Hackathon/Assets/Scripts/Main/Barista.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Assets;
+using UnityEngine.Assertions.Must;
 using UnityEngine.Networking;
 
 public class Barista : NetworkBehaviour
@@ -15,10 +16,18 @@ public class Barista : NetworkBehaviour
 
     public SyncList<string> availableActions = new SyncListString {"This", "is", "test", "actions"};
 
+
     public Barista(string playerName, List<string> baristaActions)
     {
+        var actions = GetMyActions(4);
         PlayerName = playerName;
         this.baristaActions = baristaActions;
+    }
+
+    private List<string> GetMyActions(int requestedActions)
+    {
+        var rng = new Random();
+        return new List<string>();
     }
 
     public void AssignOrder(FufilledOrder newOrder)
